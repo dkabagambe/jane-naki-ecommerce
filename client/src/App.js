b615ca41090930962896ae792e3116abd8a6cb31
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-07 13:38:15 +0300

    ecommerce mern app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Jane naki</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
