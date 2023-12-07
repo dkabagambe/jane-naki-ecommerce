d18f3761edae662d6e1d29a62765ce2e143ede3a
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-07 17:53:48 +0300

    added product page

---
+++ client/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: "Vintage  shirt",
+      slug: "vintage-shirt",
+      category: "Shirts",
+      image:
+        "https://pictures-uganda.jijistatic.com/11840153_NjIwLTgyOS02YWQ1NmQ1OWNiLTI.webp",
+      price: 25000,
+      countInStock: 10,
+      brand: "Vintage",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality Shirt",
+    },
+    {
+      name: "adidas shirt",
+      slug: "adidas-shirt",
+      category: "Shirts",
+      image:
+        "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9ca452b6c4cb496a8b01a930013eacd4_9366/3-Stripes_Tee_White_DV2824_01_laydown.jpg",
+      price: 30000,
+      countInStock: 18,
+      brand: "Adidas",
+      rating: 4.1,
+      numReviews: 12,
+      description: "easy to wear Shirt",
+    },
+    {
+      name: "Suede Classic shoe",
+      slug: "Suede-Classic-shoe",
+      category: "Shoes",
+      image:
+        "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/380561/01/sv01/fnd/PNA/fmt/png/Suede-Classic-XXI-Little-Kids'-Shoes",
+      price: 150000,
+      countInStock: 20,
+      brand: "Puma",
+      rating: 7.5,
+      numReviews: 16,
+      description: "high quality Shoes",
+    },
+    {
+      name: "Streetwear boys trousers",
+      slug: "Streetwear-boys-trousers",
+      category: "Trousers",
+      image:
+        "https://image.made-in-china.com/202f0j00tSzVusjGaCrp/New-Fashion-Streetwear-Boys-Cargo-Pants-Kids-Old-Children-Wide-Trousers.webp",
+      price: 50000,
+      countInStock: 13,
+      brand: "Streetwear",
+      rating: 4.5,
+      numReviews: 19,
+      description: "high quality Shirt",
+    },
+  ],
+};
+export default data;
