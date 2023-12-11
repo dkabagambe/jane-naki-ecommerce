91b17890b1d0e0a86c344ff93e4fb0f1e4418c7b
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-11 14:07:08 +0300

    added the image logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Product from "./screens/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "./img/logo.png";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">Jane naki</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand href="/">
+                  <img src={logo} alt=" logo" />
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
