cdaff502f30b86a45afc89aab6456b9b5c6d84c8
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-11 10:55:48 +0300

    Featured Products

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,14 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 // import data from "../data";
 import { Link } from "react-router-dom";
 import axios from "axios";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("/api/products");
-      setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
