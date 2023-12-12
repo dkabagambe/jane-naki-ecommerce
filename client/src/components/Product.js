b50642fe5c061ce5b6510cc3e5a8247ab012d1fd
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-12 13:15:35 +0300

    i have added the rating in the  components folder and reviews

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Rating from "./Rating";
 
 function Product(props) {
   const { product } = props;
@@ -9,13 +10,14 @@ function Product(props) {
       <Link to={`/product/${product.slug}`}>
         <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
-      <Card.Body>
+      <Card.Body className="product-item">
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text> Ugx:{product.price}</Card.Text>
       </Card.Body>
-      <Button>Add to cart</Button>
+      <Button className="button ">Add to cart</Button>
     </Card>
   );
 }
