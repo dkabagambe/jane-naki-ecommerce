f57e0f9b97bbe8b8cc0e926266aa8544cd92ae5a
daniel Kabagambe <danielkabagambe@gmail.com>
2023-12-21 15:34:37 +0300

    create payment method

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import CartScreen from "./screens/Cart";
 import SigninScreen from "./screens/Signin";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -26,6 +27,7 @@ function App() {
     ctxDispatch({ type: "USER_SIGNOUT" });
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress");
+    localStorage.removeItem("paymentMethod");
   };
   return (
     <BrowserRouter>
@@ -83,6 +85,7 @@ function App() {
                 path="/shipping"
                 element={<ShippingAddressScreen />}
               ></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
               <Route path="/" element={<Home />} />
             </Routes>
           </Container>
